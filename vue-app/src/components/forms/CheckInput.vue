fa215400bb533e3c1fd10c77a1fc2cdfdebeb4d7
DEVCOMP\erymn <erymnur@gmail.com>
2026-07-01 16:02:58 +0700

    Adding and using vue component from prev lesson

---
+++ vue-app/src/components/forms/CheckInput.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="form-check">
+            <input
+                        type="checkbox"
+                        :name="name"
+                        :required="required"
+                        :value="value"
+                        :id="name"
+                        :checked="checked"
+                        class="form-check-input"/>
+            <label class="form-check-label" :for="name">
+                {{label}}
+            </label>
+        </div>
+</template>
+
+<script>
+    export default{
+        name: 'CheckInput',
+        props: ["value", "name", "label", "required", "checked"],
+    }
+</script>
---
+++ vue-app/src/components/forms/SelectInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="mb-3">
+            <label v-bind:for="name" class="form-label">{{label}}</label>
+            <select :id="name" class="form-select" :name="name" :required="required">
+                <option v-for="opt in items" :value="opt.value" :key="opt.value">
+                    {{opt.text}}
+                </option>
+            </select>
+        </div>
+</template>
+
+<script>
+    export default{
+        name: 'SelectInput',
+        props: ["items", "name", "label", "required"],
+    }
+</script>
